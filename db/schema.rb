9278dcdecb2bf591b9f6081201fc4d8afc1ab2d7
Evan Moore <etmoore@gmail.com>
2014-04-22 09:27:14 -0600

    image uploading almost complete

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140421175339) do
+ActiveRecord::Schema.define(version: 20140421214841) do
 
   create_table "comments", force: true do |t|
     t.text     "body"
